66e9bbce34d5fd3298bab70034d1e82cd984d63e
nekusu <nosoynahuel11@gmail.com>
2021-07-02 05:03:47 -0300

    Improve output of the /summary command

--- plugins/summary.py
+++ plugins/summary.py
@@ -2,6 +2,7 @@ import os, matplotlib
 from json import load
 from configparser import ConfigParser
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from pyrogram import Client, filters
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -14,13 +15,50 @@ matplotlib.style.use('seaborn-dark')
 miner = flexpoolapi.miner(config['bot']['eth_address'])
 
 
+def get_all_workers(date, end_date):
+	workers = []
+
+	while date <= end_date:
+		if os.path.isfile('earnings/{}.json'.format(date)):
+			with open('earnings/{}.json'.format(date), 'r') as earnings_file:
+				earnings = load(earnings_file)
+
+			for worker in earnings['workers']:
+				if not worker in workers:
+					workers.append(worker)
+
+		date = (datetime.strptime(date, '%Y-%m-%d') + timedelta(1)).strftime('%Y-%m-%d')
+
+	return workers
+
+def initialize_plot_data(total_workers):
+	plot_data = { 'bars': {}, 'lines': {}, 'days': [] }
+
+	for worker in total_workers:
+		plot_data['bars'][worker] = []
+		plot_data['lines'][worker] = []
+
+	plot_data['lines']['Total'] = []
+	return plot_data
+
+def add_workers_earnings(summary, plot_data, earnings):
+	for worker in summary['workers']:
+		if worker in earnings['workers']:
+			summary['workers'][worker] += earnings['workers'][worker]
+			plot_data['bars'][worker].append(earnings['workers'][worker])
+			plot_data['lines'][worker].append(plot_data['lines'][worker][-1] + earnings['workers'][worker] if len(plot_data['lines'][worker]) > 0 else 0)
+		else:
+			plot_data['bars'][worker].append(0)
+			plot_data['lines'][worker].append(plot_data['lines'][worker][-1] if len(plot_data['lines'][worker]) > 0 else 0)
+
+	summary['total'] += earnings['total']
+	plot_data['lines']['Total'].append(plot_data['lines']['Total'][-1] + earnings['total'] if len(plot_data['lines']['Total']) > 0 else 0)
+
 def generate_graph(data, type, title):
-	start_month = data['days'][0].month
-	end_month = data['days'][-1].month if data['days'][-1].month != start_month else None
 	df = pd.DataFrame(data[type], index=[d.day for d in data['days']])
 	plot = df.plot.bar(rot=0, figsize=(10, 6), stacked=True) if type == 'bars' else df.plot.line(figsize=(10, 6))
 	plot.legend(loc='upper center', ncol=5, title='Workers')
-	plt.title('{}{} | {}'.format(data['days'][0].strftime('%B'), ' - {}'.format(data['days'][-1].strftime('%B')) if end_month else '', title))
+	plt.title('{} | {}'.format(data['days'][0].strftime('%B'), title))
 	plt.xlabel('Days')
 	plt.ylabel('ETH')
 	fig = plot.get_figure()
@@ -36,8 +74,7 @@ def send_graphs(data, client, chat_id):
 def summary(client, message):
 	print(message.text)
 	wait_message = client.send_message(message.chat.id, "Wait a second...")
-	reply_message = None
-	start_date = datetime.today().replace(day=1, month=datetime.today().month - 1 if datetime.today().day == 1 else datetime.today().month).strftime('%Y-%m-%d')
+	start_date = datetime.today().replace(day=1, month=datetime.today().month + (-1 if datetime.today().day == 1 else 0)).strftime('%Y-%m-%d')
 	end_date = (datetime.today() - timedelta(1)).strftime('%Y-%m-%d')
 
 	if len(message.command) > 1:
@@ -46,44 +83,37 @@ def summary(client, message):
 		end_date = message.command[2]
 
 	date = start_date
-	summary = {'workers': {}, 'total': 0.0}
-	plot_data = {'bars': {}, 'lines': {'Total': []}, 'days': []}
+	summary = { 'workers': {}, 'total': 0.0 }
+	total_workers = get_all_workers(date, end_date)
+	plot_data = initialize_plot_data(total_workers)
+
+	for worker in total_workers:
+		summary['workers'][worker] = 0
 
 	while date <= end_date:
+		end_month = (datetime.strptime(date, '%Y-%m-%d') + relativedelta(day=31)).strftime('%Y-%m-%d')
+		earnings = None
+
 		if os.path.isfile('earnings/{}.json'.format(date)):
 			with open('earnings/{}.json'.format(date), 'r') as earnings_file:
 				earnings = load(earnings_file)
 
-			for worker in earnings['workers']:
-				if not worker in summary['workers']:
-					summary['workers'][worker] = earnings['workers'][worker]
-					plot_data['bars'][worker] = []
-					plot_data['lines'][worker] = []
-				else:
-					summary['workers'][worker] += earnings['workers'][worker]
-
-				plot_data['bars'][worker].append(earnings['workers'][worker])
-				plot_data['lines'][worker].append(summary['workers'][worker])
-
-			plot_data['days'].append(datetime.strptime(date, '%Y-%m-%d'))
-			summary['total'] += earnings['total']
-			plot_data['lines']['Total'].append(summary['total'])
+		if earnings:
+			add_workers_earnings(summary, plot_data, earnings)
 		else:
-			if summary['total'] != 0.0:
-				send_graphs(plot_data, client, message.chat.id)
-				reply = get_summary(summary, start_date, date)
-				reply_message = client.edit_message_text(message.chat.id, wait_message.message_id, reply)
-
 			client.send_message(message.chat.id, "No data saved on {}.".format(date))
-			start_date = (datetime.strptime(date, '%Y-%m-%d') + timedelta(1)).strftime('%Y-%m-%d')
-			plot_data = {'bars': {}, 'lines': {'Total': []}, 'days': []}
+			plot_data['lines']['Total'].append(plot_data['lines']['Total'][-1] if len(plot_data['lines']['Total']) > 0 else 0)
+
+		plot_data['days'].append(datetime.strptime(date, '%Y-%m-%d'))
+
+		if date == end_month:
+			send_graphs(plot_data, client, message.chat.id)
+			plot_data = initialize_plot_data(total_workers)
 
 		date = (datetime.strptime(date, '%Y-%m-%d') + timedelta(1)).strftime('%Y-%m-%d')
 
-	send_graphs(plot_data, client, message.chat.id)
-	reply = get_summary(summary, start_date, end_date)
+	if plot_data != initialize_plot_data(total_workers):
+		send_graphs(plot_data, client, message.chat.id)
 
-	if reply_message:
-		client.send_message(message.chat.id, reply)
-	else:
-		client.edit_message_text(message.chat.id, wait_message.message_id, reply)
+	reply = get_summary(summary, start_date, end_date)
+	client.edit_message_text(message.chat.id, wait_message.message_id, reply)
