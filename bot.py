e8e8eb0aefea92847d413f7c3d99b25f0f33e619
nekusu <nosoynahuel11@gmail.com>
2021-07-11 15:13:11 -0300

    Add misfire_grace_time to job scheduler

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ from earnings import daily_earnings
 
 app = Client('flex_workbot')
 scheduler = BackgroundScheduler()
-scheduler.add_job(daily_earnings, 'cron', hour='0', minute='5', args=[app])
+scheduler.add_job(daily_earnings, 'cron', hour='0', minute='5', args=[app], misfire_grace_time=180)
 scheduler.start()
 app.run()
